7440d9c4438316924af742c64483c691d67ca35b
Shivkarmaholic <kharadkarsandeep1@gmail.com>
2022-06-28 14:17:02 +0530

    commit

---
+++ Training/Self/CW_5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Self
+{
+    class CW_5
+    {
+        static void Main(string[] args)
+        {
+        
+
+        }
+    }
+}
